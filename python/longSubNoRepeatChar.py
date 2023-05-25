3ed6a97040deca4952ce7042af04847f0b9eae64
johnpuksta <128403355+johnpuksta@users.noreply.github.com>
2023-05-24 20:47:55 -0400

    Length of Longest Substring with no Repeats

---
+++ python/longSubNoRepeatChar.py
@@ -0,0 +1,24 @@
+# Length Of Longest Substring - Medium Difficulty
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if (len(s) == 1):
+            return 1
+
+        retVal, endIndex, startIndex, retArr, = 0, 0, 0, []
+        for c in s:
+            endIndex += 1
+            if (c not in retArr):
+                retArr = s[startIndex:endIndex]
+
+            retVal = max(retVal, len(retArr))
+            retArr = s[startIndex:endIndex]
+
+            while ({x for x in retArr if retArr.count(x) > 1}):
+                startIndex += 1
+                retArr = retArr[1::]
+        return retVal
+
+
+t = Solution()
+r = t.lengthOfLongestSubstring("abccadda")
+p = 3
