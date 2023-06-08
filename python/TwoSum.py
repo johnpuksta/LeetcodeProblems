34c280f9aa403ac60c2c7ea4d7cd76ee31109405
johnpuksta <128403355+johnpuksta@users.noreply.github.com>
2023-06-07 20:09:41 -0400

    Found better TwoSum solutions

--- python/TwoSum.py
+++ python/TwoSum.py
@@ -1,5 +1,5 @@
 # Two Sum problem - Easy Difficulty
-# Could use improvements, bad time complexity
+# Naive approach, bad time complexity
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         temp = 0
@@ -10,6 +10,28 @@ class Solution:
                     return j, i
         return []
 
+# hashmap, faster runtime but worse memory usage
 
-t = Solution()
+
+class Solution1:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        d = {}
+        for i, j in enumerate(nums):
+            r = target - j
+            if (r in d):
+                return [i, d[r]]
+            d[j] = i
+
+# no hashmap, slower runtime but better memory usage
+
+
+class Solution2:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i, j in enumerate(nums):
+            r = target - j
+            if (r in nums and nums.index(r) != i):
+                return [i, nums.index(r)]
+
+
+t = Solution2()
 t.twoSum([0, 4, 3, 0], 0)
